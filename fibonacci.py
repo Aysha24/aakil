e80e69361838b9fbd89c47343408725dc7b9abb5
Aysha24 <36396363+Aysha24@users.noreply.github.com>
2018-05-24 14:44:18 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
-a=int(input("enter the number:"))
-n1=0
-n2=1
-for i in range(0,a):
-  sum=n1+n2
+b=int(input("enter the number:"))
+m1=0
+m2=1
+for i in range(0,b):
+  sum=m1+m2
   print (sum)
-  n1=n2
-  n2=sum
+  m1=m2
+  m2=sum
