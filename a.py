e56dfa844182f8c4a2cf04813e74c9a2966aba03
Aysha24 <36396363+Aysha24@users.noreply.github.com>
2018-05-24 16:19:52 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
-n=int(input("enter the range of numbers:"))
+n=int(input())
 l=[]
 d=[]
 for i in range(1,n+1):
-  a=int(input("enter the elements:"))
+  a=int(input())
   d=l.append(a)
   d.sort()
   print d
